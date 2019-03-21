b27a8e28ee845c1291bb2bd38608f17c38b8a95b
Chris White <chriswhite@cmwftw-2.local>
2019-03-21 11:19:09 -0700

    bug

--- recipes/init.rb
+++ recipes/init.rb
@@ -4,21 +4,26 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-
-if node[:chef_splunk][:accept_license]
-  init_start_command = "#{node[:chef_splunk][:home]}/bin/splunk start --accept-license --answer-yes"
+if ::File.exist?('/etc/systemd/system/SplunkForwarder.service')
+  splunk_service = 'SplunkForwarder'
 else
-  init_start_command = "#{node[:chef_splunk][:home]}/bin/splunk start"
+  splunk_service = 'splunk'
 end
 
-execute 'initial_splunk_start' do
-  command init_start_command
-  only_if do ::File.exists?("#{node[:chef_splunk][:home]}/ftr") end
-  notifies :run, 'execute[start_splunk_on_boot]'
+
+if node[:chef_splunk][:accept_license]
+  accept_license = "--accept-license --answer-yes"
+else
+  accept_license = ""
 end
 
 execute 'start_splunk_on_boot' do
-  command "#{node[:chef_splunk][:home]}/bin/splunk enable boot-start -user #{node[:chef_splunk][:splunk_user]}; systemctl daemon-reload"
+  command "#{node[:chef_splunk][:home]}/bin/splunk enable boot-start -user #{node[:chef_splunk][:splunk_user]} #{accept_license}; systemctl daemon-reload"
+  only_if do ::File.exists?("#{node[:chef_splunk][:home]}/ftr") end
+  notifies :start, "service[#{splunk_service}]"
+end
+
+service splunk_service do
   action :nothing
 end
 
